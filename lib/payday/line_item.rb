8eef3462acf3c6e42dfaa881fb4a66a1b6a0a460
Marcel Jackwerth <marcel@northdocks.com>
2011-04-04 00:38:59 +0200

    do not cast Money to BigDecimal (keep currency)

--- lib/payday/line_item.rb
+++ lib/payday/line_item.rb
@@ -6,24 +6,28 @@ module Payday
   # Otherwise, we'll do our best to convert the set values to a +BigDecimal+.
   class LineItem
     include LineItemable
-    
+
     attr_accessor :description, :quantity, :price
-    
+
     # Initializes a new LineItem
     def initialize(options = {})
       self.quantity = options[:quantity] || "1"
-      self.price = options[:price] || "0.00"
+      self.price = options[:price] || Money.new(0, Payday::Config.default.currency)
       self.description = options[:description] || ""
     end
-    
+
     # Sets the quantity of this {LineItem}
     def quantity=(value)
       @quantity = BigDecimal.new(value.to_s)
     end
-    
+
     # Sets the price for this {LineItem}
     def price=(value)
-      @price = BigDecimal.new(value.to_s)
+      if [BigDecimal, Money].include?(value.class)
+        @price = value
+      else
+        @price = BigDecimal.new(value.to_s)
+      end
     end
   end
 end
